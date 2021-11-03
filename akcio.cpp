dde1b7d27047001838ed6d1b0c14314a1e488eba
Sárdi András <sardi.andras@gmail.com>
2021-11-03 18:20:39 +0100

    Create akcio.cpp

---
+++ akcio.cpp
@@ -0,0 +1,13 @@
+void akcio(int ar[], int meret){
+for(int i = 0; i < meret; i++){
+    ar[i] = ar[i]*0.9;
+    }
+}
+
+/*
+ void akcio(int arak[], int db) {
+    for (int i = 0; i < db; i++) {
+        arak[i] = (int) (arak[i] * 0.9);
+    }
+}
+ */
